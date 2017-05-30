1066120d637c3ef86418d058ca5336842f11cdf1
Masternn54 <windshield2012@hotmail.com>
2017-05-30 15:19:54 +0200

    Branch safety

---
+++ app/src/main/java/awesomegroupkolond/kontraktor/MainActivity.java
@@ -0,0 +1,88 @@
+package awesomegroupkolond.kontraktor;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView yLabel = (TextView)findViewById(R.id.yLabel);
+        TextView mLabel = (TextView)findViewById(R.id.mLabel);
+        TextView dLabel = (TextView)findViewById(R.id.dLabel);
+        TextView eLabel = (TextView)findViewById(R.id.elabel);
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/mmmm/d/E", Locale.US );
+        String  strDate = sdf.format(cal.getTime());
+
+        String[] values =strDate.split("/",0);
+
+     //   for(int i = 0; i < values.length; i++){
+     //   Log.v("CHECK_DATE" , values[i]);
+     //   }
+
+
+        yLabel.setText(values[0]);
+        mLabel.setText(values[1]);
+        dLabel.setText(values[2]);
+        eLabel.setText(values[3]);
+
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+           // Made a greeting for the customer
+        Toast.makeText(getApplicationContext(), "Welcome to Kontraktor! :)", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
